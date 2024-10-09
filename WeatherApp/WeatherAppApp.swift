6574f0d5f4f05ccb7250ab01369cb03c5734c4de
Tsep1l0v <w1nd0wstsepilov@yandex.ru>
2024-10-09 12:09:37 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Дмитрий Цепилов on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
